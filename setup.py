5cc1fa36e9cbc0e6d36bce5f56eb349da1b39006
Martin Sundermeyer <Martin.Sundermeyer@dlr.de>
2020-02-10 16:55:07 +0100

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auto_pose',
-    version='0.0.28',
+    version='0.0.32',
     packages=find_packages(exclude=('docs')),
     package_data={'auto_pose': ['ae/cfg/*', 'ae/cfg_eval/*', 'ae/cfg_m3vision/*', 'meshrenderer/*', 'shader/*', 'm3_interface/sample_data/*']},
     # install_requires=['imgaug>=0.2.3'],
