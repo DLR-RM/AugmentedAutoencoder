3efd00dccd46c71e939db45d023ac943b75f7c2c
Martin Sundermeyer <Martin.Sundermeyer@dlr.de>
2020-02-10 17:09:54 +0100

    .

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='auto_pose',
     version='0.0.32',
-    packages=find_packages(exclude=('docs')),
+    packages=find_packages(exclude=('docs')) + ['auto_pose.meshrenderer.shader'],
     package_data={'auto_pose': ['ae/cfg/*', 'ae/cfg_eval/*', 'ae/cfg_m3vision/*', 'meshrenderer/gl_utils/*', 'meshrenderer/shader/*', 'meshrenderer/pysixd/*', 'm3_interface/sample_data/*']},
     # install_requires=['imgaug>=0.2.3'],
-    install_requires=['progressbar', 'bitarray'],
     author='Martin Sundermeyer, Dimitri Henkel',
     author_email='Martin.Sundermeyer@dlr.de, Dimitri.Henkel@dlr.de',
     license='DLR proprietary',
@@ -17,7 +16,7 @@ setup(
                             'ae_eval = auto_pose.eval.ae_eval:main',
                             'ae_test_embedding = auto_pose.test.ae_test_embedding:main',
                             'ae_compare = auto_pose.eval.comparative_report:main'],
-	"m3vision.models": [
+	    "m3vision.models": [
                             "auto_pose = auto_pose.m3_interface.ae_pose_estimator:AePoseEstimator"
                             ]
     },
