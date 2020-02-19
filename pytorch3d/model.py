c6ff2aa629f57f599b22890f2ec3fa9fbdff7cdf
Stefan Hein Bengtson <shbe@create.aau.dk>
2020-02-19 10:08:26 +0100

    removed unused code from the model and add reference

--- pytorch3d/model.py
+++ pytorch3d/model.py
@@ -3,7 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# Network structure inspired by:
+# https://arxiv.org/pdf/1708.05628.pdf (see fig. 2)
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -20,16 +21,7 @@ class Model(nn.Module):
     # Input: x = lantent code
     # Output: y = pose as quaternion
     def forward(self,x):
-        #x = F.relu(self.bn1(self.l1(x)))
-        #x = F.relu(self.bn2(self.l2(x)))
-        #x = F.relu(self.l1(x))
-        #x = F.relu(self.l2(x))
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-
-        #x = self.l1(x)
-        #x = self.l2(x)
-        
+        x = F.relu(self.bn1(self.l1(x)))
+        x = F.relu(self.bn2(self.l2(x)))                
         y = self.tanh(self.l3(x))
-        #y = self.l3(x)
         return y
