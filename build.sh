c87788bc7151fc98d8fb6c9508363fcfbf0cd76e
Martin Sundermeyer <martin.sundermeyer@dlr.de>
2020-06-17 21:02:45 +0200

    removed local files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-unset PYTHONPATH
-
-SCRIPT_DIR=`pwd`
-INSTALLFOLDER=${SCRIPT_DIR}/install/${DLRRM_HOST_PLATFORM}
-export PYTHONUSERBASE=${INSTALLFOLDER}
-
-/usr/bin/pip2 install --upgrade --user .
